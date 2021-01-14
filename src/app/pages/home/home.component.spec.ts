7f4a749b73a1f967e5841e82775ef097af44d216
Ana Camila Santos Sousa <ana.c.santos.sousa@avanade.com>
2021-01-14 18:12:49 -0300

    Ajustes nos testes unitários

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
 
 import { HomeComponent } from './home.component';
 
@@ -8,7 +11,14 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      imports: [ 
+        HttpClientModule 
+      ],
+      providers: [
+        FormBuilder
+      ],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   });
--- src/app/services/fipe-cars.service.spec.ts
+++ src/app/services/fipe-cars.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { FipeCarsService } from './fipe-cars.service';
@@ -6,7 +7,9 @@ describe('FipeCarsService', () => {
   let service: FipeCarsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientModule ],
+    });
     service = TestBed.inject(FipeCarsService);
   });
 
